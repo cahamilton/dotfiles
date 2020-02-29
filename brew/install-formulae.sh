311e6e916957c602a1e8ade7b86065986d3a4251
Carl Hamilton <carlanthonyhamilton@gmail.com>
2020-02-29 19:16:39 +0000

    Add Homebrew formulae installer

---
+++ brew/install-formulae.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Development
+brew install git
+brew install git-extras
+brew install nvm
+brew install php
+brew install python
+brew install yarn
+
+# Utilities
+brew install coreutils
+brew install fdupes
+brew install ffmpeg
+brew install gotop
+brew install moreutils
+brew install rsync
+brew install z
+brew install zsh
+brew install zsh-autosuggestions
+
+# Novelty
+brew install gti
