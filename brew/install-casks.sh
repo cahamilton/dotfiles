bbaeac388f312450d8e0ebc928de8911d2fb9f3b
Carl Hamilton <4007311+cahamilton@users.noreply.github.com>
2020-02-29 19:20:27 +0000

    Add Homebrew cask installer

---
+++ brew/install-casks.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+# Add additional repositories
+brew tap homebrew/cask-drivers
+brew tap homebrew/cask-fonts
+
+# Browsers
+brew cask install firefox
+brew cask install firefox-nightly
+brew cask install google-chrome
+brew cask install google-chrome-canary
+brew cask install microsoft-edge-canary
+brew cask install opera
+brew cask install safari-technology-preview
+
+# Development
+brew cask install adobe-creative-cloud
+brew cask install charles
+brew cask install cyberduck
+brew cask install docker
+brew cask install font-meslo-for-powerline
+brew cask install font-meslo-lg
+brew cask install github
+brew cask install imagealpha
+brew cask install imageoptim
+brew cask install iterm2
+brew cask install jetbrains-toolbox
+brew cask install postman
+brew cask install sequel-pro
+brew cask install sketch
+brew cask install sublime-text
+brew cask install virtualbox
+brew cask install visual-studio-code
+brew cask install webponize
+
+# Utilities
+brew cask install 1password
+brew cask install alfred
+brew cask install authy
+brew cask install backblaze
+brew cask install backblaze-downloader
+brew cask install bettertouchtool
+brew cask install disk-inventory-x
+brew cask install dropbox
+brew cask install flux
+brew cask install gpg-suite
+brew cask install handbrake
+brew cask install istumbler
+brew cask install keepingyouawake
+brew cask install onyx
+brew cask install openoffice
+brew cask install private-internet-access
+brew cask install the-unarchiver
+brew cask install viscosity
+
+# Entertainment
+brew cask install plex-media-player
+brew cask install sony-ps4-remote-play
+brew cask install spotify
+brew cask install steam
+brew cask install twitch
+brew cask install vlc
+
+# Social
+brew cask install caprine
+brew cask install discord
+brew cask install slack
+brew cask install telegram
+brew cask install whatsapp
+
+# Quick Look Plugins
+brew cask install qlcolorcode
+brew cask install qlimagesize
+brew cask install qlmarkdown
+brew cask install qlprettypatch
+brew cask install qlstephen
+brew cask install qlvideo
+brew cask install quicklook-csv
+brew cask install quicklook-json
+brew cask install webpquicklook
