126d39f8a7c3d658747eb67a90a1b42d41eb3258
Carl Hamilton <carlanthonyhamilton@gmail.com>
2020-02-29 19:12:47 +0000

    Add Homebrew base installer

---
+++ brew/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Check if Homebrew is installed
+if test ! ["$(command -v brew)"]; then
+	echo "Installing Homebrew..."
+
+	# Install Homebrew
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+	echo "Updating Homebrew and formulae..."
+
+	# Update to latest Homebrew
+	brew update
+
+	# Upgrade existing formulae
+	brew upgrade
+fi
+
+# Remove stale formulae and casks
+brew cleanup
