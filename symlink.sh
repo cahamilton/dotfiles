c31517095608f1242a03f6416721c47ec4bab18b
Carl Hamilton <carlanthonyhamilton@gmail.com>
2021-02-16 17:44:34 +0000

    Update .zshrc

---
+++ symlink.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+ln -sf ~/.dotfiles/.aliases ~/.aliases
+ln -sf ~/.dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
