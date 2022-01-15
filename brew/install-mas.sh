e563fe198c8019f8a8d6c0818666c57abb347644
Carl Hamilton <4007311+cahamilton@users.noreply.github.com>
2022-01-15 22:15:04 +0000

    Update Mac App Store list

--- brew/install-mas.sh
+++ brew/install-mas.sh
@@ -9,8 +9,9 @@ mas upgrade
 
 # Install listed applications
 mas install 1278508951 # Trello
-mas install 408981434 # iMovie
-mas install 409183694 # Keynote
-mas install 409201541 # Pages
-mas install 409203825 # Numbers
-mas install 497799835 # Xcode
+mas install 408981434  # iMovie
+mas install 409183694  # Keynote
+mas install 409201541  # Pages
+mas install 409203825  # Numbers
+mas install 497799835  # Xcode
+mas install 905953485  # NordVPN
