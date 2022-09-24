1a572a948a8d98d3704e2eefa1de751714421036
Carl Hamilton <4007311+cahamilton@users.noreply.github.com>
2022-09-24 12:54:40 +0100

    Remove chained cask installer

--- brew/install-casks.sh
+++ brew/install-casks.sh
@@ -5,88 +5,82 @@ brew tap homebrew/cask-drivers
 brew tap homebrew/cask-versions
 
 # Browsers
-brew install --cask \
-	brave-browser \
-	firefox \
-	firefox-developer-edition \
-	google-chrome \
-	google-chrome-dev \
-	microsoft-edge \
-	microsoft-edge-dev \
-	opera \
-	safari-technology-preview
+brew install brave-browser --cask
+brew install firefox --cask
+brew install firefox-developer-edition --cask
+brew install google-chrome --cask
+brew install google-chrome-dev --cask
+brew install microsoft-edge --cask
+brew install microsoft-edge-dev --cask
+brew install opera --cask
+brew install safari-technology-preview --cask
 
 # Development
-brew install --cask \
-	adobe-creative-cloud \
-	altair-graphql-client \
-	browserstacklocal \
-	ccmenu \
-	docker \
-	figma \
-	github \
-	imageoptim \
-	iterm2 \
-	jetbrains-toolbox \
-	postman \
-	sequel-pro \
-	sketch \
-	sublime-text \
-	virtualbox \
-	visual-studio-code \
-	webponize
+brew install adobe-creative-cloud --cask
+brew install altair-graphql-client --cask
+brew install browserstacklocal --cask
+brew install ccmenu --cask
+brew install docker --cask
+brew install figma --cask
+brew install github --cask
+brew install imageoptim --cask
+brew install iterm2 --cask
+brew install jetbrains-toolbox --cask
+brew install postman --cask
+brew install sequel-pro --cask
+brew install sketch --cask
+brew install sublime-text --cask
+brew install virtualbox --cask
+brew install visual-studio-code --cask
+brew install webponize --cask
 
 # Utilities
-brew install --cask \
-	1password \
-	alfred \
-	appcleaner \
-	authy \
-	backblaze \
-	backblaze-downloader \
-	bettertouchtool \
-	cakebrew \
-	disk-inventory-x \
-	dropbox \
-	flux \
-	gpg-suite-no-mail \
-	handbrake \
-	keepingyouawake \
-	logitech-options \
-	netgear-switch-discovery-tool \
-	onyx \
-	skitch \
-	the-unarchiver \
-	viscosity
+brew install 1password --cask
+brew install alfred --cask
+brew install appcleaner --cask
+brew install authy --cask
+brew install backblaze --cask
+brew install backblaze-downloader --cask
+brew install bettertouchtool --cask
+brew install cakebrew --cask
+brew install disk-inventory-x --cask
+brew install dropbox --cask
+brew install flux --cask
+brew install gpg-suite-no-mail --cask
+brew install handbrake --cask
+brew install keepingyouawake --cask
+brew install logitech-options --cask
+brew install netgear-switch-discovery-tool --cask
+brew install onyx --cask
+brew install skitch --cask
+brew install the-unarchiver --cask
+brew install viscosity --cask
 
 # Entertainment
-brew install --cask \
-	lastfm \
-	plex-media-player \
-	spotify \
-	steam \
-	twitch \
-	vlc
+brew install lastfm --cask
+brew install plex-media-player --cask
+brew install spotify --cask
+brew install steam --cask
+brew install twitch --cask
+brew install vlc --cask
 
 # Social
-brew install --cask \
-	discord \
-	messenger \
-	microsoft-teams \
-	signal \
-	slack \
-	telegram \
-	whatsapp \
-	zoom
+brew install discord --cask
+brew install messenger --cask
+brew install microsoft-teams --cask
+brew install signal --cask
+brew install slack --cask
+brew install telegram --cask
+brew install whatsapp --cask
+brew install zoom --cask
 
 # Quick Look Plugins
-brew install --cask \
-	qlcolorcode \
-	qlimagesize \
-	qlmarkdown \
-	qlprettypatch \
-	qlstephen \
-	qlvideo \
-	quicklook-csv \
-	quicklook-json \
-	webpquicklook
+brew install qlcolorcode --cask
+brew install qlimagesize --cask
+brew install qlmarkdown --cask
+brew install qlprettypatch --cask
+brew install qlstephen --cask
+brew install qlvideo --cask
+brew install quicklook-csv --cask
+brew install quicklook-json --cask
+brew install webpquicklook --cask
