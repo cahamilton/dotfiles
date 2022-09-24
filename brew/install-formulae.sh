e4014f86828c454a38c86b924124013e90caa5e2
Carl Hamilton <4007311+cahamilton@users.noreply.github.com>
2022-09-24 12:49:57 +0100

    Remove chained formulae installer

--- brew/install-formulae.sh
+++ brew/install-formulae.sh
@@ -1,27 +1,23 @@
 #!/usr/bin/env bash
 
 # Development
-brew install \
-	commitizen \
-	gh \
-	git \
-	git-extras \
-	hadolint \
-	nvm \
-	openjdk@11 \
-	pyenv
+brew install commitizen
+brew install gh
+brew install git
+brew install git-extras
+brew install hadolint
+brew install openjdk@11
+brew install pyenv
 
 # Utilities
-brew install \
-	coreutils \
-	fdupes \
-	htop \
-	moreutils \
-	neofetch \
-	rsync \
-	zplug \
-	zsh
+brew install coreutils
+brew install fdupes
+brew install htop
+brew install moreutils
+brew install neofetch
+brew install rsync
+brew install zplug
+brew install zsh
 
 # Novelty
-brew install \
-	gti
+brew install gti
