75bb994d4c22c6683addbc811d229ee1306bf203
Carl Hamilton <4007311+cahamilton@users.noreply.github.com>
2024-12-23 20:50:51 +0000

    Update formulae

--- brew/install-formulae.sh
+++ brew/install-formulae.sh
@@ -6,15 +6,17 @@ brew install gh
 brew install git
 brew install git-extras
 brew install hadolint
+brew install ollama
 brew install openjdk@11
 brew install pyenv
 
 # Utilities
+brew install asimov
 brew install coreutils
 brew install fdupes
 brew install htop
+brew install jq
 brew install moreutils
-brew install neofetch
 brew install rsync
 brew install zplug
 brew install zsh
