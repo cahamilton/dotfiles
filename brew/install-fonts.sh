0b375dbbfc8863fdf31be84117adf37241b6bebd
Carl Hamilton <4007311+cahamilton@users.noreply.github.com>
2025-07-02 21:17:45 +0100

    Update fonts

--- brew/install-fonts.sh
+++ brew/install-fonts.sh
@@ -3,9 +3,11 @@
 # Install fonts
 brew install font-fira-code --cask
 brew install font-hack-nerd-font --cask
+brew install font-lato --cask
 brew install font-meslo-for-powerline --cask
 brew install font-meslo-lg --cask
 brew install font-meslo-lg-nerd-font --cask
+brew install font-roboto-slab --cask
 brew install font-ubuntu --cask
 brew install font-ubuntu-condensed --cask
 brew install font-ubuntu-mono --cask
